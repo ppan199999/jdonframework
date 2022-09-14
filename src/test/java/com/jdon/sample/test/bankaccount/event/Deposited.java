9c132b5c7d0763867fe4adbbe0d63770b11ea786
ppan199999 <j7jfha@gmail.com>
2022-09-14 08:18:58 +0000

    2ZHOU

--- src/test/java/com/jdon/sample/test/bankaccount/event/Deposited.java
+++
@@ -1,8 +0,0 @@
-package com.jdon.sample.test.bankaccount.event;
-
-public class Deposited extends TransferEvent {
-
-	public Deposited(String transactionId, String aggregateId, String eventId, int value) {
-		super(transactionId, aggregateId, eventId, value);
-	}
-}
